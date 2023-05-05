a37c909c5c6ed5be0d3bde3959c7244c91f5c4af
Francis Miyoba <francismiyoba82@gmail.com>
2023-05-05 10:55:07 +0300

    first commit

---
+++ src/layouts/components/detail/CustomerDetail.tsx
@@ -0,0 +1,194 @@
+// ** React Imports
+import {ChangeEvent, forwardRef, useState} from 'react'
+
+// ** MUI Imports
+import Grid from '@mui/material/Grid'
+import Radio from '@mui/material/Radio'
+import Select from '@mui/material/Select'
+import Button from '@mui/material/Button'
+import MenuItem from '@mui/material/MenuItem'
+import TextField from '@mui/material/TextField'
+import FormLabel from '@mui/material/FormLabel'
+import InputLabel from '@mui/material/InputLabel'
+import RadioGroup from '@mui/material/RadioGroup'
+import CardContent from '@mui/material/CardContent'
+import FormControl from '@mui/material/FormControl'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import FormControlLabel from '@mui/material/FormControlLabel'
+
+// ** Third Party Imports
+import DatePicker from 'react-datepicker'
+
+// ** Styled Components
+import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
+
+const CustomInput = forwardRef((props, ref) => {
+  return <TextField inputRef={ref} label='Birth Date' fullWidth {...props} />
+})
+
+interface State {
+  title: string
+  firstName: string
+  lastName: string
+  nrc: string
+  employeeNumber: string
+  primaryPhoneNumber: string
+  secondaryPhoneNumber: string
+  district: string
+  department: string
+  institution: string
+  createdBy: string
+  createdOn: string
+}
+
+const CustomerDetail = () => {
+  // ** State
+  const [date, setDate] = useState<Date | null | undefined>(null)
+  const [values, setValues] = useState<State>({
+    title: '',
+    firstName: '',
+    lastName: '',
+    nrc: '',
+    employeeNumber: '',
+    primaryPhoneNumber: '',
+    secondaryPhoneNumber: '',
+    district: '',
+    department: '',
+    institution: '',
+    createdBy: '',
+    createdOn: ''
+  });
+  const handleChange = (prop: keyof State) => (event: ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [prop]: event.target.value })
+  }
+ console.log(`FirstName is ${values.firstName}`)
+  return (
+    <CardContent>
+      <form>
+        <Grid container spacing={7}>
+
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel>Title</InputLabel>
+              <Select label='Title' defaultValue='Mrs' value={values.title} onChange={handleChange('title')}>
+                <MenuItem value='Mr'>Mr</MenuItem>
+                <MenuItem value='Ms'>Ms</MenuItem>
+                <MenuItem value='Mrs'>Mrs</MenuItem>
+                <MenuItem value='Miss'>Miss</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label='FirstName'
+              value={values.firstName}
+              onChange={handleChange('firstName')}
+              placeholder='First Name'
+              defaultValue='Some Name'
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label='LastName'
+              value={values.lastName}
+              onChange={handleChange('lastName')}
+              placeholder='Last Name'
+              defaultValue='Some Name'
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label='NRC'
+              value={values.nrc}
+              onChange={handleChange('nrc')}
+              placeholder='NRC'
+              defaultValue='Some NRC'
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label='Employee Number'
+              value={values.employeeNumber}
+              onChange={handleChange('employeeNumber')}
+              placeholder='Employee Number'
+              defaultValue='Some Num'
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label='District'
+              value={values.district}
+              onChange={handleChange('district')}
+              placeholder='District'
+              defaultValue='Some Num'
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label='Department'
+              placeholder='Department'
+              value={values.department}
+              onChange={handleChange('department')}
+              defaultValue='Some Num'
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label='Institution'
+              value={values.institution}
+              onChange={handleChange('institution')}
+              placeholder='Institution'
+              defaultValue='Some Num'
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label='Created By'
+              value={values.createdBy}
+              onChange={handleChange('createdBy')}
+              placeholder='Created By'
+              defaultValue='Some Num'
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <DatePickerWrapper>
+              <DatePicker
+                selected={date}
+                showYearDropdown
+                showMonthDropdown
+                id='account-settings-date'
+                placeholderText='MM-DD-YYYY'
+                customInput={<CustomInput />}
+                onChange={(date: Date) => setDate(date)}
+              />
+            </DatePickerWrapper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField fullWidth type='number' label='Phone' placeholder='(123) 456-7890' />
+          </Grid>
+
+
+
+          <Grid item xs={12}>
+            <Button variant='contained' sx={{ marginRight: 3.5 }}>
+              Save Changes
+            </Button>
+            <Button type='reset' variant='outlined' color='secondary' onClick={() => setDate(null)}>
+              Reset
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </CardContent>
+  )
+}
+
+export default CustomerDetail
---
+++ src/pages/pages/agents/index.tsx
@@ -0,0 +1,148 @@
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import Chip from '@mui/material/Chip'
+import Table from '@mui/material/Table'
+import TableRow from '@mui/material/TableRow'
+import TableHead from '@mui/material/TableHead'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import Typography from '@mui/material/Typography'
+import TableContainer from '@mui/material/TableContainer'
+
+// ** Types Imports
+import { ThemeColor } from 'src/@core/layouts/types'
+
+
+import { getFirestore, collection } from 'firebase/firestore';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import firebase from '../../../firebase/config'
+
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+interface RowType {
+  name: string
+  district: string
+  phone: string
+}
+
+interface StatusObj {
+  [key: string]: {
+    color: ThemeColor
+  }
+}
+
+const rows: RowType[] = [
+
+]
+
+const statusObj: StatusObj = {
+  applied: { color: 'info' },
+  rejected: { color: 'error' },
+  accepted: { color: 'success' }
+}
+
+
+
+const AgentsPage = () => {
+  const router = useRouter()
+
+  // function handleRowClick(agentID) {
+  //   router.push(`/agents/${agentID}`) // navigate to dynamic route for employee
+  // }
+  const [value, loading, error] = useCollection(
+    collection(getFirestore(firebase), 'users')
+  );
+
+  const newData: {
+    id: string;
+    name: string;
+    email: string;
+    phoneNumber: string;
+    orderCount: number;
+    customerCount: number;
+    deleteCount: number;
+    dateJoined: { seconds: number; nanoseconds: number };
+    lastName: string;
+    firstName: string;
+    district: string;
+  }[] = [];
+
+  value?.forEach((doc) => {
+    const data = doc.data();
+    console.log(`OUR USERS ${JSON.stringify(data)}`);
+    const {
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      orderCount,
+      customerCount,
+      deleteCount,
+      dateJoined,
+      district,
+      ...rest
+    } = data;
+    const name = `${firstName} ${lastName}`;
+    newData.push({
+      id: doc.id,
+      name,
+      email,
+      phoneNumber,
+      orderCount,
+      customerCount,
+      deleteCount,
+      dateJoined,
+      lastName,
+      firstName,
+      district
+    });
+  });
+
+
+
+  if (loading) {
+    return 'loading...'
+  }
+
+  if (error) {
+    return `Error fetching data: ${error}`
+  }
+
+
+  //console.log(`Our value is ${JSON.stringify(value)}`)
+
+  return (
+    <Card>
+      <TableContainer>
+        <Table sx={{ minWidth: 800 }} aria-label='table in dashboard'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>District</TableCell>
+              <TableCell>Phone Number</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody >
+            {newData.map((row) => (
+              <TableRow onClick={() => router.push(`/pages/agents/${row.id}`)} hover key={row.name} sx={{ '&:last-of-type td, &:last-of-type th': { border: 0 } }}>
+                <TableCell
+                  sx={{ py: theme => `${theme.spacing(0.5)} !important` }}
+                >
+                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Typography sx={{ fontWeight: 500, fontSize: '0.875rem !important' }}>{row.name}</Typography>
+                  </Box>
+                </TableCell>
+                <TableCell>{row.district}</TableCell>
+                <TableCell>{row.phoneNumber}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Card>
+  )
+}
+
+export default AgentsPage
